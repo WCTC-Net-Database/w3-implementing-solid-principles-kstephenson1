b0f80dfa32a567f32695e59fa6aad035e6b64de7
Kyle S <kstephenson1@my.wctc.edu>
2025-02-07 00:09:37 -0600

    Renamed Interface to UserInterface

---
+++ UserInterface.cs
@@ -0,0 +1,30 @@
+﻿namespace w3_assignment_ksteph
+{
+    // The UI class contains elements for the UI including the main menu and the exit message.
+    public static class UserInterface
+    {
+        public static void MainMenu()
+        {
+            Console.WriteLine(
+                "╔═════════════════════════╗\n" +
+                "║        Main Menu        ║\n" +
+                "╠═════════════════════════╣\n" +
+                "║1. Display Characters    ║\n" +
+                "║2. New Character         ║\n" +
+                "║3. Level Up Character    ║\n" +
+                "║4. Exit                  ║\n" +
+                "╚═════════════════════════╝\n");
+        }
+
+        public static void ExitMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                "\n\n\n\n\n\n\n\n\n" +
+                "          ╔═════════════════════════════════════════════════════╗\n" +
+                "          ║    Thank you for using the RPG Character Editor.    ║\n" +
+                "          ╚═════════════════════════════════════════════════════╝\n" +
+                "\n\n\n\n\n\n\n\n\n");
+        }
+    }
+}
