4e5110955d53bfb2390c5d9d4349100df0e873f1
Kyle S <kstephenson1@my.wctc.edu>
2025-02-07 00:09:07 -0600

    Refactored inventory and rewrote string to itemlist to inventory conversions to make them make sense

---
+++ Inventory/Inventory.cs
@@ -0,0 +1,11 @@
+﻿namespace w3_assignment_ksteph.Inventory;
+public class Inventory
+{
+    public List<Item>? Items { get; set; } = new();
+
+    public Inventory() { }
+    public Inventory(List<Item> items)
+    {
+        Items = items;
+    }
+}
---
+++ Inventory/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace w3_assignment_ksteph.Inventory;
+
+public class Item
+{
+    public string Name { get; set; }
+
+    public Item(string name)
+    {
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
