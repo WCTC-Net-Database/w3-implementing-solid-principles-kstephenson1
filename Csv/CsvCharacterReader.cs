3b31ee5b9518eb4df5a23c256467327c62ba9583
kstephenson <kstephenson1@my.wctc.edu>
2025-02-08 23:23:44 -0600

    Create CsvCharacterReader.cs
    Created CsvCharacterReader class to import characters to conform with SRP

---
+++ Csv/CsvCharacterReader.cs
@@ -0,0 +1,27 @@
+﻿namespace w3_assignment_ksteph.Csv;
+
+using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using w3_assignment_ksteph.Character;
+
+public static class CsvCharacterReader
+{  
+    public static List<Character> Import(string path)
+    {
+
+        List<Character> characters = new();
+      
+        using StreamReader reader = new(path);
+        using CsvReader csv = new(reader, CultureInfo.InvariantCulture);
+
+        IEnumerable<CsvCharacterIO> csvCharacters = csv.GetRecords<CsvCharacterIO>();
+
+        foreach (CsvCharacterIO csvCharacter in csvCharacters)
+        {
+            characters.Add(csvCharacter.ToCharacter());
+        }
+
+      return characters;
+    }
+}
