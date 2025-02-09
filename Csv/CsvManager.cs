e6ff668b2b157b5c81b82faffec80a4c07edd4ee
Kyle S <kstephenson1@my.wctc.edu>
2025-02-09 01:01:53 -0600

    Added CsvToCharacter and CharacterToCsv

--- Csv/CsvManager.cs
+++ Csv/CsvManager.cs
@@ -1,17 +1,19 @@
 ﻿namespace w3_assignment_ksteph.Csv;
 
+using w3_assignment_ksteph.Character;
+using w3_assignment_ksteph.Inventory;
 public static class CsvManager
 {
-    public Character CsvToCharacter(CsvCharacterIO csvCharacter)
+    public static Character CsvToCharacter(CsvCharacterIO csvCharacter)
     {
         return new(csvCharacter.Name, csvCharacter.Class, csvCharacter.Level, csvCharacter.HitPoints, InventoryManager.ToInventory(csvCharacter.Inventory));
     }
 
-    public CsvCharacterIO CharactertoCsv(Character character)
+    public static CsvCharacterIO CharactertoCsv(Character character)
     {
-        return new(character.Name, character.Class, character.Level, character.HitPoints, InventoryManager.ToString(character.Inventory));
+        return new() { Name = character.Name, Class = character.Class, Level = character.Level, HitPoints = character.HitPoints, Inventory = InventoryManager.ToString(character.Inventory), };
     }
     
-    public static void ImportCharacters(string path) => CsvCharacterReader(path);
-    public static void ExportCharacters(List<Character> characters, string path) => CsvCharacterWriter(characters, path);
+    public static void ImportCharacters(string path) => CsvCharacterReader.Import(path);
+    public static void ExportCharacters(List<Character> characters, string path) => CsvCharacterWriter.Export(characters, path);
 }
