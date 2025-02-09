cd0c67a1249154b53cfc58c76a10a65211e8e409
Kyle S <kstephenson1@my.wctc.edu>
2025-02-09 01:21:49 -0600

    Added conversion from Character to CsvCharacter and reimplemented CsvHelper correctly.

--- Csv/CsvCharacterWriter.cs
+++ Csv/CsvCharacterWriter.cs
@@ -1,6 +1,7 @@
 ﻿namespace w3_assignment_ksteph.Csv;
 
 using CsvHelper;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using w3_assignment_ksteph.Character;
@@ -10,12 +11,17 @@ public static class CsvCharacterWriter
 
     public static void Export(List<Character> characters, string path)
     {
-        using StreamWriter writer = new(path);
-        using CsvWriter csvOut = new(writer, CultureInfo.InvariantCulture);
+        List<CsvCharacterIO> outputCharacters = new();
 
         foreach (Character character in characters)
         {
-            csvOut.WriteRecords(character.ToCsvCharacterIO());
+            outputCharacters.Add(CsvManager.CharactertoCsv(character));
         }
+
+
+        using StreamWriter writer = new(path);
+        using CsvWriter csvOut = new(writer, CultureInfo.InvariantCulture);
+
+        csvOut.WriteRecords(outputCharacters);
     }
 }
