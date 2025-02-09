7102154467b731673c99f2a693437d26525e6fac
kstephenson <kstephenson1@my.wctc.edu>
2025-02-08 23:47:31 -0600

    Update CsvManager.cs
    Added conversion of CsvCharacter to Character and vice versa

--- Csv/CsvManager.cs
+++ Csv/CsvManager.cs
@@ -2,6 +2,16 @@
 
 public static class CsvManager
 {
+    public Character CsvToCharacter(CsvCharacterIO csvCharacter)
+    {
+        return new(csvCharacter.Name, csvCharacter.Class, csvCharacter.Level, csvCharacter.HitPoints, InventoryManager.ToInventory(csvCharacter.Inventory));
+    }
+
+    public CsvCharacterIO CharactertoCsv(Character character)
+    {
+        return new(character.Name, character.Class, character.Level, character.HitPoints, InventoryManager.ToString(character.Inventory));
+    }
+    
     public static void ImportCharacters(string path) => CsvCharacterReader(path)
     public static void ExportCharacters(List<Character> characters, string path) => CsvCharacterWriter(characters, path)
 }
