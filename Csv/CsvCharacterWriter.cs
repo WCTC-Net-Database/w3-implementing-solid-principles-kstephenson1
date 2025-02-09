9769c4cbd702569615c3d4186a6b70d34ea5b6ab
kstephenson <kstephenson1@my.wctc.edu>
2025-02-08 23:26:09 -0600

    Create CsvCharacterWriter.cs
    Created CsvCharacterWriter to conform with SRP

---
+++ Csv/CsvCharacterWriter.cs
@@ -0,0 +1,21 @@
+﻿namespace w3_assignment_ksteph.Csv;
+
+using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
+using w3_assignment_ksteph.Character;
+
+public static class CsvCharacterWriter
+{
+
+    public static void Export(List<Character> characters, string path)
+    {
+        using StreamWriter writer = new(path);
+        using CsvWriter csvOut = new(writer, CultureInfo.InvariantCulture);
+
+        foreach (Character character in characters)
+        {
+            csvOut.WriteRecords(character.ToCsvCharacterIO());
+        }
+    }
+}
