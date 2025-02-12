34ae95fa499cd7df420ea38b1bbf8e8a0c8a6d89
kstephenson <kstephenson1@my.wctc.edu>
2025-02-11 21:50:36 -0600

    Create InventoryConverter.cs
    Created InventoryConverter

---
+++ Csv/InventoryConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace w3_assignment_ksteph.Csv;
+
+// The InventoryConverter is used to turn the csv string into an Inventory Object
+public class InventoryConverter : DefaultTypeConverter
+{
+    public override object ConvertFromString(string text, IRowReaderRow row, MemberMapData memberMapData)
+    {
+        return InventoryManager.ToInventory(text);
+    }
+}
