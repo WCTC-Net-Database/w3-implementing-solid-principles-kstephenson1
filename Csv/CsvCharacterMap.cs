de36be8a5a6a2970b4b0442f2bd12f3ffcc0abba
kstephenson <kstephenson1@my.wctc.edu>
2025-02-11 21:43:47 -0600

    Create CsvCharacterMap.cs
    Created CsvCharacterMap

---
+++ Csv/CsvCharacterMap.cs
@@ -0,0 +1,16 @@
+﻿namespace w3_assignment_ksteph.Csv;
+
+// The Character Map is used to convert character information from csv format to a Character object.
+public class CsvCharacterMap : CsvClassMap<Character>
+{
+    public CsvCharacterMap()
+    {
+        InventoryConverter inventoryConveter = new InventoryConverter()
+    
+        Map(character => character.Name);
+        Map(character => character.Class);
+        Map(character => character.Level);
+        Map(character => character.HitPoints);
+        Map(character => character.Inventory).TypeConverter(inventoryConverter);
+    }
+}
