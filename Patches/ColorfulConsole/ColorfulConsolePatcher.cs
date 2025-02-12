818b99d74c37ab25ab8130763ad9fc620f560531
kstephenson <kstephenson1@my.wctc.edu>
2025-02-11 23:16:32 -0600

    Update ColorfulConsolePatcher.cs
    Added more comments to patcher

--- Patches/ColorfulConsole/ColorfulConsolePatcher.cs
+++ Patches/ColorfulConsole/ColorfulConsolePatcher.cs
@@ -11,7 +11,18 @@ public class ColorfulConsolePatcher
          * Colorful.Console has a bug that results in the colors being generated in an specific order instead of as named.
          * This NuGet package is broken but this line of code seems to fix the colors.  After some testing, for some reason,
          * after initially using the colors in this exact specific order seems to fix the issue.  I would be better off using
-         * the built in console colors instead of this package.
+         * the built in console colors instead of this package.  The issue seems to have something to do with Windows 11 Update 22H.
+         *
+         * For example in this sample:
+         *     Console.WriteLine("Line 1",Color.Red);
+         *     Console.WriteLine("Line 2",Color.White);
+         *     Console.WriteLine("Line 3",Color.Blue);
+         *     Console.WriteLine("Line 4",Color.Red);
+         *
+         * Line 1 would show as Blue, Line 2 would show as Green, and Line 3 would show as Cyan, and Line 4 would be Blue because it
+         * was already assigned.  Whatever color you use first gets assigned the color Blue, the second used color is always Green,
+         * and etc.  Using these colors in the correct order at the beginning of the program seems to fix the issue enough for use
+         * in this program.
          */
 
         List<Color> colors = new()
