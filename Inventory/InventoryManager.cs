0b146a4837062895e3b5e09d2b51d5b96bf93b6a
kstephenson <kstephenson1@my.wctc.edu>
2025-02-11 22:04:38 -0600

    Update InventoryManager.cs
    Moved most of the methods here to InventorySerializer

--- Inventory/InventoryManager.cs
+++ Inventory/InventoryManager.cs
@@ -2,90 +2,9 @@
 
 using System;
 using System.Collections.Generic;
+using w3_assignment_ksteph.Csv;
 
 public class InventoryManager
 {
-    public static void ListInventory(Inventory inventory) // Takes the Inventory, turns it into stringInventory, and displays the inventory to the user.
-    {
-        ListInventory(ToString(inventory));
-    }
-
-    public static void ListInventory(string inventoryString) // Takes the inventory string, splits it, and displays the inventory to the user.
-    {
-        Console.WriteLine($"Inventory:");
-
-        if (inventoryString == "" || inventoryString == null)
-        {
-            Console.WriteLine("    - (Empty)");
-        }
-        else
-        {
-            List<Item> items = ToItemList(inventoryString);
-
-            foreach (Item item in items)
-            {
-                Console.WriteLine($"    - {item}");
-            }
-
-            Console.WriteLine("\n");
-        }
-    }
-
-    public static List<Item> ToItemList(string itemString)
-    {
-        //Converts String into List<Item>
-        List<Item> itemList = [];
-
-        string[] items = itemString.Split('|');
-
-        foreach (string item in items)
-            itemList.Add(new Item(item));
-
-        return itemList;
-    }
-
-    public static string ToString(List<Item> items)
-    {
-        // Converts List<Item> to String
-        if (items == null)
-            return "";
-        else
-        {
-            string inventory = "";
-
-            foreach (Item item in items)
-            {
-                if (inventory == "")
-                    inventory += item.ToString();
-                else
-                    inventory += "|" + item.ToString();
-            }
-
-            return inventory;
-        }
-    }
-
-    public static Inventory ToInventory(List<Item> itemList)
-    {
-        // Converts List<Item> to Inventory
-        return new(itemList);
-    }
-
-    public static List<Item>? ToItemList(Inventory inventory)
-    {
-        // Converts Inventory to List<Item>
-        return inventory.Items;
-    }
-
-    public static Inventory ToInventory(string inventoryString)
-    {
-        // Converts String into Inventory
-        return ToInventory(ToItemList(inventoryString));
-    }
-
-    public static string ToString(Inventory inventory)
-    {
-        // Converts Inventory into String
-        return ToString(ToItemList(inventory));
-    }
+    public static void ListInventory(Inventory inventory) => InventorySerializer.ListInventory(ToString(inventory));
 }
