d3add1dfa0193de3844c768e7d34d45951f92b2b
Kyle S <kstephenson1@my.wctc.edu>
2025-02-13 02:23:26 -0600

    Added functions to assist with TitleCase

---
+++ DataHelper/StringHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace w3_assignment_ksteph.DataHelper;
+
+// The DataHelper.StringFuctions class contains string manipulation methods.
+class StringHelper
+{
+    /// <summary>
+    /// Takes the string and returns a variant in Titlecase
+    /// </summary>
+    /// <param name="input">The string to be converted to Titlecase</param>
+    /// <returns>Uppercase <strong>String</strong> provided by the user.</returns>
+    public static string ToTitleCase(string input) => CultureInfo.InvariantCulture.TextInfo.ToTitleCase(input);
+}
