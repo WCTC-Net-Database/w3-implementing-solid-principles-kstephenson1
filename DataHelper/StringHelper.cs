5a8c6d5a3072f749cd87e42a035b03a9ad6a38cb
Kyle S <kstephenson1@my.wctc.edu>
2025-02-13 02:42:30 -0600

    Added itemid and itemname string conversions

--- DataHelper/StringHelper.cs
+++ DataHelper/StringHelper.cs
@@ -1,9 +1,10 @@
 ﻿using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace w3_assignment_ksteph.DataHelper;
 
 // The DataHelper.StringFuctions class contains string manipulation methods.
-class StringHelper
+public static class StringHelper
 {
     /// <summary>
     /// Takes the string and returns a variant in Titlecase
@@ -11,4 +12,6 @@ class StringHelper
     /// <param name="input">The string to be converted to Titlecase</param>
     /// <returns>Uppercase <strong>String</strong> provided by the user.</returns>
     public static string ToTitleCase(string input) => CultureInfo.InvariantCulture.TextInfo.ToTitleCase(input);
+    public static string ToItemIdFormat(string text) => text.Replace(" ", "_").ToLower();
+    public static string ToItemNameFormat(string text) => ToTitleCase(text.Replace("_", " ").ToLower());
 }
